666272bc374e60acf95e0d7a6cf5ea4f19ce02d2
Marlon Reis <marlon-reis91@hotmail.com>
2026-06-24 14:46:47 -0300

    feat(domain): Adicionar interfaces de repositório
    - Criada interface ICandidatoRepository em src/ATS.Domain/Candidatos/Repositories
    - Criada interface ICandidaturaRepository em src/ATS.Domain/Candidaturas/Repositories
    - Criada interface IVagaRepository em src/ATS.Domain/Vagas/Repositories
    - Definidos contratos para operações de persistência e consulta assíncrona
    - Mantida separação clara entre domínio e infraestrutura (DDD)

---
+++ src/ATS.Domain/Vagas/Repositories/IVagaRepository.cs
@@ -0,0 +1,12 @@
+namespace ATS.Domain.Vagas.Repositories;
+
+using ATS.Domain.Vagas.Entities;
+
+public interface IVagaRepository
+{
+    Task<Vaga?> ObterPorIdAsync(Guid id, CancellationToken ct = default);
+    Task<IEnumerable<Vaga>> ListarAsync(int pagina, int tamanhoPagina, CancellationToken ct = default);
+    Task AdicionarAsync(Vaga vaga, CancellationToken ct = default);
+    Task AtualizarAsync(Vaga vaga, CancellationToken ct = default);
+    Task RemoverAsync(Guid id, CancellationToken ct = default);
+}
